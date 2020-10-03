a8fbc6f480e45dca46eb7427ffa07fb869d337b6
linhtran <linhtran@Linhs-MacBook-Air.local>
2020-10-03 17:29:34 -0500

    Merge branch 'master' of /Users/linhtran/SaseHackathon with conflicts.

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -5,11 +5,57 @@ class ProfilePage extends StatefulWidget {
   _ProfilePageState createState() => _ProfilePageState();
 }
 
+// class _ProfilePageState extends State<ProfilePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Text("This is Profile page"),
+//     );
+//   }
+// }
+
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("This is Profile page"),
+    return Scaffold(
+      body: Container(
+        color: Colors.deepPurpleAccent,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.deepPurpleAccent,
+                    Colors.purple,
+                  ]
+                )
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.only(top: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white, width: 2.0),
+                      ),
+                      padding: EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://64.media.tumblr.com/01e33dfa2edc2ad7069567262bffe7b3/tumblr_pf3ryqq7So1v5pqjv_540.png"),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
