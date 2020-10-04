0db5c99f5487e6081695088473110306ec4d788c
stavo41 <jimenez.gustavo41@gmail.com>
2020-10-03 20:03:31 -0500

    added bubbles

--- lib/screens/maps.dart
+++ lib/screens/maps.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapsPage extends StatelessWidget {
   @override
@@ -16,33 +18,59 @@ class MapSample extends StatefulWidget {
 
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
+  Set<Circle> _circles = HashSet<Circle>();
+
+  @override
+  void initState() {
+    super.initState();
+    _setCircles();
+  }
+
+  void _setCircles() {
+    _circles.add(
+      Circle(
+          circleId: CircleId("0"),
+          center: LatLng(37.76493, -122.42432),
+          radius: 200,
+          strokeWidth: 2,
+          fillColor: Colors.red[300].withOpacity(0.5)),
+      // fillColor: Color.fromRGBO(102, 51, 153, .5)),
+    );
+    _circles.add(
+      Circle(
+          circleId: CircleId("1"),
+          center: LatLng(37.75493, -122.42432),
+          radius: 500,
+          strokeWidth: 2,
+          fillColor: Colors.red[300].withOpacity(0.5)),
+      // fillColor: Color.fromRGBO(102, 51, 153, .5)),
+    );
+    _circles.add(
+      Circle(
+          circleId: CircleId("2"),
+          center: LatLng(37.75493, -122.43432),
+          radius: 250,
+          strokeWidth: 2,
+          fillColor: Colors.red[300].withOpacity(0.5)),
+      // fillColor: Color.fromRGBO(102, 51, 153, .5)),
+    );
+  }
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.77483, -122.41942),
-    zoom: 14.4746,
+    zoom: 13,
   );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
+        circles: _circles,
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
